45d4798582609a5edc303f737734f74037280f33
Sucheta Kar <karsucheta47@gmail.com>
2024-04-02 03:19:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "test-ec2-instance-1" {
+resource "aws_instance" "test-ec2-instance" {
   ami                     = "ami-0a70b9d193ae8a799"
   instance_type           = "m5.large"
 }
