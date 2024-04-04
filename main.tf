2d65c9e5d7834c3ede5e7336531cad28964dcae1
Sucheta Kar <karsucheta47@gmail.com>
2024-04-04 09:54:51 +0530

    Testing pull request

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "test-ec2-instance" {
+resource "aws_instance" "test-ec2-instance1" {
   ami                     = "ami-0a70b9d193ae8a799"
   instance_type           = "m5.large"
 }
